2d8abe8e82a5065f6a5645a34972adeba65d33f3
Franziska <Franziska@PC1996.localdomain>
2018-03-25 15:37:31 +0200

    Aufgabe00

---
+++ Aufgabe 00/Aufgabe00.ts
@@ -0,0 +1,10 @@
+namespace Aufgabe00 {
+        
+function Textbox(): void {
+  var name: string = prompt("Bitte gib deinen Namen ein?", "");
+  if (name != null) {
+     document.getElementById("Name").innerHTML = "Hi " + name + ". Hier gibts noch nichts spannendes zu sehen.";
+     }
+  }
+    
+}
